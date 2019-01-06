e182d8b6678c7c13be835a9721c4ad3bdfb18339
albert <amontescobo@gmail.com>
2019-01-06 22:02:22 +0100

    Implemented random number of character types. Still need to implement whole password generation

---
+++ password_generator.rb
@@ -0,0 +1,20 @@
+module PasswordGenerator
+	NUMBER_OF_CHARACTERS = 16	
+	def PasswordGenerator.generateRandomPass
+		allowed_chars = {
+			:lowercase => ['a'..'z'].to_a,
+			:uppercase => ['A'..'Z'].to_a,
+			:numbers => ['0'..'9'].to_a,
+			:symbols => ['!', '"', '#', "\$", '%', '&', "\'", "\(", "\)", '*', '+', "\,", '-', "\.", '/', "\:", 
+						 "\;", '<', "\=", '>', '?', "\@", '[', "\\", "\]", '^', "\_", "\'", "\{", '|', "\}", "\~"]
+						 }
+			num_of_lowercase = rand(1..NUMBER_OF_CHARACTERS - 3)
+			num_of_uppercase = rand(1..NUMBER_OF_CHARACTERS - num_of_lowercase - 2)
+			num_of_numbers = rand(1..NUMBER_OF_CHARACTERS - num_of_lowercase - num_of_uppercase - 1)
+			num_of_symbols = rand(1..NUMBER_OF_CHARACTERS - num_of_lowercase - num_of_uppercase - num_of_numbers)
+			pass = allowed_chars[:lowercase].shuffle[0:num_of_lowercase] + allowed_chars[:uppercase].shuffle[0:num_of_uppercase] + 
+			allowed_chars[:numbers].shuffle[0:num_of_numbers] + allowed_chars[:symbols].shuffle[0:num_of_symbols]
+			puts "Generated password: #{pass}"
+	end
+	PasswordGenerator.generateRandomPass
+end
